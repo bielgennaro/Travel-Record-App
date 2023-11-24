700fb60ccea0e52eb32fd86a8070e81bb9f87437
Gabriel Zirondi Di Gennaro <zirondigabriel@gmail.com>
2023-11-23 23:02:39 -0300

    Add project files.

---
+++ TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, System.EventArgs e)
+        {
+            var isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            var isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+                DisplayAlert("Ops!", "Please enter both email and password", "OK");
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
