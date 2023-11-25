f5cd5925ddc4a7b428ca0604f7fe0e1373927b84
Gabriel Zirondi Di Gennaro <zirondigabriel@gmail.com>
2023-11-25 19:48:18 -0300

    Adding tabbed pages

---
+++ TravelRecordApp/MapPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
